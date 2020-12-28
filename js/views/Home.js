2b4a78afbbc5f26edff4df6c4ff35658f5ce2cef
Sami Hattab <hattab_sami@yahoo.fr>
2020-12-28 12:21:58 +0100

    homepage router is functionnal

---
+++ js/views/Home.js
@@ -0,0 +1,28 @@
+export default function () {
+
+    $("#root").append(`
+    <h1>Articles Généraux</h1>
+    <section id="ctnr"></section>
+    `)
+    
+    $.ajax({
+        url: 'http://cors-anywhere.herokuapp.com/' + 'http://newsapi.org/v2/top-headlines?country=fr&apiKey=20bd631a035141d1bb5cebb9b6529e1d',
+        dataType: "json"
+    })
+        .then((result) => {
+            console.log("resultats=", result.articles)
+
+            $.each(result.articles, function (index, result) {
+                $("#ctnr").append(
+                    `<article>
+                        <img src="${result.urlToImage}" alt="">
+                        <div>
+                            <h2>${result.title}</h2>
+                            <p>${result.description}</p>
+                        </div>
+                    </article>`
+                )
+            })
+        })
+}
+
