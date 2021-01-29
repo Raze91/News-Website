73b19c7e1c75c7df674b954d93125436b7503f85
Sami Hattab <hattab_sami@yahoo.fr>
2021-01-29 09:31:52 +0100

    add error management in article.js

--- js/views/Article.js
+++ js/views/Article.js
@@ -16,9 +16,9 @@ export default function (params) {
         <section class="article">
             <h1>${selectedArticle.title}</h1>
             <p>Par ${(selectedArticle.author != null? selectedArticle.author : "Auteur inconnu")} le ${selectedArticle.publishedAt}</p>
-            <img src="${selectedArticle.urlToImage}" alt="">
-            <p>${selectedArticle.content}</p>
-            <p>${selectedArticle.description}</p>
+            <img src="${selectedArticle.urlToImage !== null ? selectedArticle.urlToImage : "assets/imageNotFound.png"}" alt="">
+            <p>${selectedArticle.content !== null ? selectedArticle.content : "Pas de contenu pour cet article"}</p>
+            <p>${selectedArticle.description !== null ? selectedArticle.description : "Pas de Description pour cet article"}</p>
             <a href="${selectedArticle.url}" target="_blank">Lien vers l'article original</a>
         </section>
         `)
